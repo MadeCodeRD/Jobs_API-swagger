d529338d846619ca7f54b08c5f13aeece6b18633
MadeCodeRD <Madelson.Acosta2@gmail.com>
2022-09-29 16:34:42 -0400

    swagger docs added

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI= require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const authenticateUser = require('./middleware/authentication');
 
 //Connect db
@@ -40,8 +45,10 @@ app.use(xss());
 
 // routes
 app.get('/', (req, res)=>{
-  res.send('Jobs API');
+  res.send('<h1>Jobs API</h1><a href="/api-docs">DOCUMENTATION</a>');
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs',authenticateUser, jobsRouter);
 
